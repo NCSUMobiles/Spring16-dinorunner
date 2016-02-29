9f07dca7fd9f92ff442e04e5a2886d420623e3be
Jimin Fan <jfan4@ncsu.edu>
2016-02-29 00:26:10 -0500

    Added DinoPicker View, allows player to pick a from a list of dinosaurs, includes option to add images

---
+++ mobile/src/main/java/com/dino/ncsu/dinorunner/DinoPickerActivity.java
@@ -0,0 +1,13 @@
+package com.dino.ncsu.dinorunner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DinoPickerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dino_picker);
+    }
+}
---
+++ mobile/src/main/java/com/dino/ncsu/dinorunner/Player.java
@@ -0,0 +1,7 @@
+package com.dino.ncsu.dinorunner;
+
+/**
+ * Created by jianpingfan on 2/28/2016.
+ */
+public class Player {
+}
