1937203290ef90b1897029f7af6a6fc41bff40cb
kjcommon138 <kjcommon@ncsu.edu>
2016-03-01 12:21:09 -0500

    Added player stats to player class

--- mobile/src/main/java/com/dino/ncsu/dinorunner/Player.java
+++ mobile/src/main/java/com/dino/ncsu/dinorunner/Player.java
@@ -4,5 +4,50 @@ package com.dino.ncsu.dinorunner;
  * Created by jiminfan on 2/28/2016.
  */
 public class Player {
+    private double topSpeed;
+    private double avgSpeed;
+    private double totalWeekDist;
+    private double totalMonthDist;
 
+    public Player() {
+    }
+
+    public Player(double topSpeed, double avgSpeed, double totalWeekDist, double totalMonthDist) {
+        setTopSpeed(topSpeed);
+        setAvgSpeed(avgSpeed);
+        setTotalWeekDist(totalWeekDist);
+        setTotalMonthDist(totalMonthDist);
+    }
+
+    public double getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(double topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public void setAvgSpeed(double avgSpeed) {
+        this.avgSpeed = avgSpeed;
+    }
+
+    public double getTotalWeekDist() {
+        return totalWeekDist;
+    }
+
+    public void setTotalWeekDist(double totalWeekDist) {
+        this.totalWeekDist = totalWeekDist;
+    }
+
+    public double getTotalMonthDist() {
+        return totalMonthDist;
+    }
+
+    public void setTotalMonthDist(double totalMonthDist) {
+        this.totalMonthDist = totalMonthDist;
+    }
 }
